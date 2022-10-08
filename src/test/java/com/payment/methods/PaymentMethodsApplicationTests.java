11095b29f51cc1ffb6dda72693eba4332b523dc4
awais2075 <awais2075@gmail.com>
2022-10-08 22:30:53 +0500

    initial commit

---
+++ src/test/java/com/payment/methods/PaymentMethodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.methods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentMethodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
